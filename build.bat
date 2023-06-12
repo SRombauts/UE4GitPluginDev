b5d25cc83c880a00db5a663c4e3443efc571c02d
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2023-06-12 12:34:03 +0200

    Update to UE5.2

--- build.bat
+++ build.bat
@@ -1,22 +1,23 @@
 @echo off
-setlocal
+setlocal EnableDelayedExpansion
 
 set ROOTPATH=%~dp0
 
 pushd %ROOTPATH%
 
+REM Default to Unreal Engine 5, but can be overriden to Sources from Github or to UE4
 if [%1] == [] (
-  set /p ENGINE="Select the version of the Engine to build with [4/5]: "
+  set ENGINE=5
 ) else (
   set ENGINE=%1
 )
-if [%ENGINE%] == [] (
-  echo Engine version is empty
-  exit /b 1
-) else if "%ENGINE%" == "4" (
-  set UBT="C:\Program Files\Epic Games\UE_4.27\Engine\Binaries\DotNET\UnrealBuildTool.exe"
+
+if "%ENGINE%" == "4" (
+  set ENGINEPATH="C:\Program Files\Epic Games\UE_4.27"
+  set UBT=!ENGINEPATH!\Engine\Binaries\DotNET\UnrealBuildTool.exe
 ) else if "%ENGINE%" == "5" (
-  set UBT="C:\Program Files\Epic Games\UE_5.1\Engine\Binaries\DotNET\UnrealBuildTool\UnrealBuildTool.exe"
+  set ENGINEPATH="C:\Program Files\Epic Games\UE_5.2"
+  set UBT=!ENGINEPATH!\Engine\Binaries\DotNET\UnrealBuildTool\UnrealBuildTool.exe
 ) else (
   echo Engine version '%ENGINE%' need to be 4 or 5
   exit /b 1
@@ -27,6 +28,8 @@ if not exist %UBT% (
     exit /b
 )
 
+echo Unsing Unreal Engine from %ENGINEPATH%
+
 for %%a in (*.uproject) do set "UPROJECT=%CD%\%%a"
 if not defined UPROJECT (
     echo *.uproject file not found
@@ -36,6 +39,9 @@ if not defined UPROJECT (
 for %%i in ("%UPROJECT%") do (
   set PROJECT=%%~ni
 )
+
+echo Generate Project Files for %UPROJECT% (project '%PROJECT%')
+
 
 echo on
 %UBT% %UPROJECT% Win64 Development %PROJECT%Editor
